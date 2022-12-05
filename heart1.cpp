9315ff68168e4e1fe54c34e8c3623abfdd0d8af3
rajat_vishwa <rajatvishwa283@gmail.com>
2022-12-05 20:17:04 +0530

    Made it work!

--- heart1.cpp
+++ heart1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int x=40;
+int x=41;
 int y=30;
 
 int main() {
@@ -8,44 +8,44 @@ int main() {
     for(int i=1;i<=y;i++){
         for(int j=1;j<=x;j++){
             if(j<=x/4 && i<=y/3){
-                if(i==j+y/3){
-                    cout<<"#";
+                if(i==-j+y/3){
+                    cout<<"# ";
                 }else{
-                    cout<<".";
+                    cout<<". ";
                 }
             }else{
                 if(j<=x/2 && i<=y/3){
-                    if(i==-j-y/3){
-                        cout<<"#";
+                    if(i==+j-y/3){
+                        cout<<"# ";
                     }else{
-                        cout<<".";
+                        cout<<". ";
                     }                                        
                 }else{
-                    if(j<=3/(4*x) && i<=y/3){
-                        if(i==j+y){
-                            cout<<"#";        
+                    if(j<= 3*x/4 && i<=y/3){
+                        if(i==-j+y){
+                            cout<<"# ";        
                         }else{
-                            cout<<".";                            
+                            cout<<". ";                            
                         }
                     }else{
                         if(i<=y/3){
-                            if(i==-j-y){
-                                cout<<"#";                            
+                            if(i==j-y){
+                                cout<<"# ";                            
                             }else{
-                                cout<<".";
+                                cout<<". ";
                             }
                         }else{
                             if(j<=x/2){
-                                if(i==-j+y/3){
-                                    cout<<"#";
+                                if(i==j+y/3){
+                                    cout<<"# ";
                                 }else{
-                                    cout<<".";
+                                    cout<<". ";
                                 }
                             }else{
-                                if(i==j+(5*y)/3){
-                                    cout<<"#";
+                                if(i==-j+(5*y)/3){
+                                    cout<<"# ";
                                 }else{
-                                    cout<<".";
+                                    cout<<". ";
                                 }
                             }
                         }
@@ -53,7 +53,7 @@ int main() {
                 }
             }
         }
-        cout<<"\n"<<endl;
+        cout<<"\n";
     }
     return 0;
 }
